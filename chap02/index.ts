6ec276d7749e12ad80ef66bedbefbef1aec14955
ShiramizuJunya <jyunya.pg.7231@gmail.com>
2023-04-25 07:38:36 +0900

    【add】letによる変数宣言と変数への代入
    ’

---
+++ chap02/index.ts
@@ -0,0 +1,41 @@
+/*
+ *
+ */
+
+// ----------------------------------
+//
+// ----------------------------------
+
+// ->
+// >
+
+/*
+ * 2.1.1 文と式の基本
+ */
+
+// ----------------------------------
+// 'Hello, 'などは文字列リテラルの式
+// const 変数名 = 式;
+// console.log(式 + 式);
+// ----------------------------------
+// const greeting = 'Hello, ';　// -> 文
+// const target = 'world';　// -> 文
+// console.log(greeting + target);　// -> 式文
+
+/*
+ * 2.2.3 型注釈（type annotation）
+ */
+// const greeting: string = 'Hello, ';
+// const target: string = 'world';
+// const target: string = 100;
+// console.log(greeting + target);
+
+/*
+ * 2.2.4
+ */
+// const greeting: string = 'Hello, ';
+// greeting = 'World';
+// let greeting: string;
+// let target: string;
+// greeting = 'Hello, ';
+// console.log(greeting + target);
