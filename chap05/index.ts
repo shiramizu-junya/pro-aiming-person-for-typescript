b76da46804782147b0f1d91cfaba033846f07489
Shiramizu_Junya <jyunya.pg.7231@gmail.com>
2023-05-17 08:26:46 +0900

    【add】第5.2章のコード

--- chap05/index.ts
+++ chap05/index.ts
@@ -2,6 +2,8 @@
  *
  */
 
+import { type } from "os";
+
 // ----------------------------------
 //
 // ----------------------------------
@@ -247,3 +249,104 @@
 /*
  * 5.2.1
  */
+// class User {
+//     name: string = '';
+//     age: number = 0;
+
+//     public isAdult(): boolean {
+//         return this.age >= 20;
+//     }
+// }
+
+// const hyo: User = new User();
+
+// const john: User = {
+//     name: 'John Smith',
+//     age: 15,
+//     isAdult: () => {
+//         return true;
+//     },
+// }
+
+// const User = class {
+// 	name: string = '';
+// 	age: number = 10;
+
+// 	isAdult(): boolean {
+// 		return this.age >= 20;
+// 	}
+// };
+
+// const uhyo = new User();
+// const john: User = new User();
+
+// class User<T> {
+//     name: string;
+//     #age: number;
+//     readonly data: T;
+
+//     constructor(name: string, age: number, data: T) {
+//         this.name = name;
+//         this.#age = age;
+//         this.data = data;
+//     }
+// }
+
+// const uhyo: User<string> = new User('uhyo', 20, '追加データ');
+
+/*
+ * 5.2.2
+ */
+// class User {
+//     name: string = 'hoge';
+//     age: number = 0;
+// }
+
+// type MyUserConstructor = new () => User;
+
+// const MyUser: MyUserConstructor = User;
+// const u = new MyUser();
+// console.log(u.name, u.age);
+
+/*
+ * 5.2.3
+ */
+// class User {
+//     name: string = 'hoge';
+//     age: number = 0;
+// }
+
+// const uhyo = new User();
+// console.log(uhyo instanceof User);
+// console.log({} instanceof User);
+
+type HasAge = {
+    age: number;
+}
+
+class User {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+function getPrice(customer: HasAge) {
+    if (customer instanceof User) {
+        if (customer.name === 'uhyo') {
+            return 0;
+        }
+    }
+    return customer.age < 18 ? 1000 : 1800;
+}
+
+const customer1: HasAge = { age: 15 };
+const customer2: HasAge = { age: 40 };
+const uhyo = new User('uhyo', 26);
+
+console.log(getPrice(customer1));
+console.log(getPrice(customer2));
+console.log(getPrice(uhyo));
