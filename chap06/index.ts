fa556c6ddaec7a98f683e75cc9792319e89c16a1
Shiramizu_Junya <jyunya.pg.7231@gmail.com>
2023-05-30 08:19:26 +0900

    【add】第6章最後まで

--- chap06/index.ts
+++ chap06/index.ts
@@ -176,15 +176,237 @@
 /*
  * 6.2.2
  */
-function getHelloStr(): `Hello, ${string}!` {
-    const rand = Math.random();
-    if (rand < 0.3) {
-        return 'Hello, world!';
-    } else if (rand < 0.6) {
-        return 'Hello, my world!';
-    } else if (rand < 0.9) {
-        return 'Hello, world.';
-    } else {
-        return 'Hell, world!';
-    }
-}
+// function getHelloStr(): `Hello, ${string}!` {
+//     const rand = Math.random();
+//     if (rand < 0.3) {
+//         return 'Hello, world!';
+//     } else if (rand < 0.6) {
+//         return 'Hello, my world!';
+//     } else if (rand < 0.9) {
+//         return 'Hello, world.';
+//     } else {
+//         return 'Hell, world!';
+//     }
+// }
+
+/*
+ * 6.3.1
+ */
+// type SignType = 'plus' | 'minus';
+
+// function signNumber(type: SignType) {
+//     return type === 'plus' ? 1 : -1;
+// }
+
+// function numberWithSign(num: number, type: SignType | 'none') {
+//     if (type === 'none') {
+//         return 0;
+//     } else {
+//         return num * signNumber(type);
+//     }
+// }
+
+/*
+ * 6.3.3
+ */
+// type Animal = {
+//     tag: 'animal';
+//     species: string;
+// }
+
+// type Human = {
+//     tag: 'human';
+//     name: string;
+// }
+
+// type User = Animal | Human;
+
+// function getName(user: User) {
+//     if (user.tag === 'human') return user.name;
+//     return '名無し';
+// }
+
+// const tama: User = {
+//     tag: 'animal',
+//     species: 'cats'
+// }
+
+// const uhyo: User = {
+//     tag: 'human',
+//     name: 'uhyo',
+// }
+
+// const alis: User = {
+//     tag: 'alis',
+//     name: 'gray'
+// }
+
+/*
+ * 6.3.4
+ */
+// type Animal = {
+//     tag: 'animal';
+//     species: string;
+// }
+
+// type Human = {
+//     tag: 'human';
+//     name: string;
+// }
+
+// type Robot = {
+//     tag: 'robot';
+//     name: string;
+// }
+
+// type User = Animal | Human | Robot;
+
+// function getName(user: User): string {
+//     // if (user.tag === 'human') return user.name;
+//     // return '名無し';
+//     switch (user.tag) {
+//         case 'human':
+//             return user.name;
+//         case 'animal':
+//             return '名無し';
+//     }
+// }
+
+/*
+ * 6.4.1
+ */
+// type Human = {
+//     type: 'human';
+//     name: string;
+//     age: number;
+// }
+
+// function setAge(human: Human, age: Human['age']) {
+//     return {
+//         ...human,
+//         age
+//     };
+// }
+
+// const uhyo: Human = {
+//     type: 'human',
+//     name: 'uhyo',
+//     age: 20,
+// }
+
+// const uhyo2 = setAge(uhyo, 30);
+// console.log(uhyo2);
+
+/*
+ * 6.4.1
+ */
+// type Human = {
+//     type: 'human';
+//     name: string;
+//     age: number;
+// }
+
+// type HumanKeys = keyof Human;
+
+// let key: HumanKeys = 'name';
+
+// key = 'age';
+
+// key = 'hoge';
+
+// const mmConversionTable = {
+//     mm: 1,
+//     m: 1e3,
+//     km: 1e6,
+// }
+
+// function convertUnits(value: number, unit: keyof typeof mmConversionTable) {
+//     const mmValue = value * mmConversionTable[unit];
+//     return {
+//         mm: mmValue,
+//         m: mmValue / 1e3,
+//         km: mmValue / 1e6
+//     }
+// }
+
+// console.log(convertUnits(5600, 'm'));
+
+/*
+ * 6.4.3
+ */
+// function get<T, K extends keyof T>(obj: T, key: K): T[K] {
+//     return obj[key];
+// }
+
+// type Human = {
+//     name: string;
+//     age: number;
+// }
+
+// const uhyo: Human = {
+//     name: 'uhyo',
+//     age: 25,
+// }
+
+// const uhyoName = get(uhyo, 'name');
+
+/*
+ * 6.4.4
+ */
+// type Obj = {
+//     0: string;
+//     1: number;
+// }
+
+// const obj: Obj = {
+//     0: 'uhyo',
+//     '1': 25,
+// }
+
+// obj['0'] = 'john';
+// obj[1] = 10;
+
+// type ObjKeys = keyof Obj;
+
+/*
+ * 6.5.1
+ */
+// function getFirstFiveLetters(strOrNum: string | number) {
+//     const str = strOrNum as string;
+//     return str.slice(0, 5);
+// }
+
+// console.log(getFirstFiveLetters('uhyohyohyo'));
+// console.log(getFirstFiveLetters(123456));
+
+/*
+ * 6.5.2
+ */
+// const name1 = ['uhyo', 'john', 'taro'];
+// const name2 = ['uhyo', 'john', 'taro'] as const;
+
+/*
+ * 6.6.1
+ */
+// function doWhatever(obj: any) {
+//     console.log(obj.user.name);
+//     obj();
+//     const result = obj * 10;
+//     return result;
+// }
+
+// doWhatever(3);
+
+// doWhatever({
+//     user: {
+//         name: 'uhyo',
+//     }
+// });
+
+// doWhatever(() => {
+//     console.log('hi');
+// });
+
+/*
+ * 6.6.3
+ */
